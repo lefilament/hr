488ff1f9afffc538cba820589de42e202c5060ae
Hugo Trentesaux <hugo@le-filament.com>
2025-11-05 11:45:18 +0100

    refac to create new view rather than patch existing

--- hr_work_entry_validate/static/src/views/calendar/calendar_controller.esm.js
+++ hr_work_entry_validate/static/src/views/calendar/calendar_controller.esm.js
@@ -1,51 +1,48 @@
-import { patch } from "@web/core/utils/patch";
-import { CalendarController } from "@web/views/calendar/calendar_controller";
+import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
 import { _t } from "@web/core/l10n/translation";
+import { WorkEntryCalendarView, WorkEntryCalendarController } from "@hr_work_entry_contract/views/work_entry_calendar/work_entry_calendar_controller";
 
-patch(CalendarController.prototype, {
+export class ValidateWorkEntryCalendarController extends WorkEntryCalendarController {
   setup() {
     super.setup(...arguments);
     this.notification = useService("notification");
     // console.log(this);
-  },
+  }
 
   // only display button if month or week
-  get workEntryDisplayButton() {
+  get displayValidateButton() {
     return this.model.meta.scale == "week" || this.model.meta.scale == "month";
-  },
+  }
 
   // tell if any record is still draft
-  get workEntryAnyDraft() {
-    return this.workEntryFilteredRecords.length != 0;
-  },
+  get anyDraft() {
+    return this.filteredRecords.length != 0;
+  }
 
   // get current draft records
-  get workEntryFilteredRecords() {
-    return this.workEntryFilterRecords(
+  get filteredRecords() {
+    return this.filterRecords(
       this.model.data.records,
-      this.model.meta.scale,
-      this.model.meta.date,
       "draft"
     );
-  },
+  }
 
   // filter records based on current month / week and state
-  workEntryFilterRecords(records, scale, date, state) {
-    const start = date.startOf(scale);
-    const stop = date.endOf(scale);
+  filterRecords(records, state) {
+    const {start, end} = this.model.computeRange()
     // filter records
     return Object.values(records).filter(
       (record) =>
         record.start > start &&
-        record.end < stop &&
+        record.end < end &&
         record.rawRecord.state == state
     );
-  },
+  }
 
   // call action_validate on current records
-  async workEntryValidate() {
-    const record_ids = this.workEntryFilteredRecords.map((r) => r.id);
+  async validate() {
+    const record_ids = this.filteredRecords.map((r) => r.id);
     console.log("Validating records", record_ids);
     const success = await this.orm.call("hr.work.entry", "action_validate", [
       record_ids,
@@ -54,10 +51,13 @@ patch(CalendarController.prototype, {
       // refresh
       this.model.env.searchModel.search();
       // notify success
-      this.notification.add(_t("%s work entries validated", record_ids.length), {
-        title: "Ok",
-        type: "success",
-      });
+      this.notification.add(
+        _t("%s work entries validated", record_ids.length),
+        {
+          title: "Ok",
+          type: "success",
+        }
+      );
     } else {
       // notify failure
       this.notification.add(_t("Work entries could not be validated."), {
@@ -65,5 +65,13 @@ patch(CalendarController.prototype, {
         type: "danger",
       });
     }
-  },
-});
+  }
+}
+
+export const ValidateWorkEntryCalendarView = {
+  ...WorkEntryCalendarView,
+  Controller: ValidateWorkEntryCalendarController,
+  buttonTemplate: "hr_work_entry_validate.calendar.controlButtons",
+};
+
+registry.category("views").add("validate_work_entries_calendar", ValidateWorkEntryCalendarView);
