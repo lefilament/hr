7eba2d0e32f4fbeef3a1dac3edcfed89895bf26c
Hugo Trentesaux <hugo@le-filament.com>
2025-11-05 16:50:09 +0100

    patch work entry calendar view instead of creating new
    hides button if user does not have rights
    eslint sort import fails but I do not understand why

--- hr_work_entry_validate/static/src/views/calendar/calendar_controller.esm.js
+++ hr_work_entry_validate/static/src/views/calendar/calendar_controller.esm.js
@@ -1,32 +1,36 @@
-import {
-    WorkEntryCalendarController,
-    WorkEntryCalendarView,
-} from "@hr_work_entry_contract/views/work_entry_calendar/work_entry_calendar_controller";
 import {_t} from "@web/core/l10n/translation";
-import {registry} from "@web/core/registry";
+import {onWillStart} from "@odoo/owl";
+import {patch} from "@web/core/utils/patch";
+import {user} from "@web/core/user";
 import {useService} from "@web/core/utils/hooks";
+import {WorkEntryCalendarController} from "@hr_work_entry_contract/views/work_entry_calendar/work_entry_calendar_controller";
 
-export class ValidateWorkEntryCalendarController extends WorkEntryCalendarController {
+patch(WorkEntryCalendarController.prototype, {
     setup() {
         super.setup(...arguments);
         this.notification = useService("notification");
+        this.isHrUser = false;
         // Console.log(this);
-    }
+
+        onWillStart(async () => {
+            this.isHrUser = await user.hasGroup("hr.group_hr_user");
+        });
+    },
 
     // Only display button if month or week
     get displayValidateButton() {
         return this.model.meta.scale === "week" || this.model.meta.scale === "month";
-    }
+    },
 
     // Tell if any record is still draft
     get anyDraft() {
         return this.filteredRecords.length !== 0;
-    }
+    },
 
     // Get current draft records
     get filteredRecords() {
         return this.filterRecords(this.model.data.records, "draft");
-    }
+    },
 
     // Filter records based on current month / week and state
     filterRecords(records, state) {
@@ -38,7 +42,7 @@ export class ValidateWorkEntryCalendarController extends WorkEntryCalendarContro
                 record.end < end &&
                 record.rawRecord.state === state
         );
-    }
+    },
 
     // Call action_validate on current records
     async validate() {
@@ -62,15 +66,5 @@ export class ValidateWorkEntryCalendarController extends WorkEntryCalendarContro
                 type: "danger",
             });
         }
-    }
-}
-
-export const ValidateWorkEntryCalendarView = {
-    ...WorkEntryCalendarView,
-    Controller: ValidateWorkEntryCalendarController,
-    buttonTemplate: "hr_work_entry_validate.calendar.controlButtons",
-};
-
-registry
-    .category("views")
-    .add("validate_work_entries_calendar", ValidateWorkEntryCalendarView);
+    },
+});
